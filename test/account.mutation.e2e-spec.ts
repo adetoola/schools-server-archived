0f252ea2fd846d14224fa3875c3866cc4d4b5c3d
Adetola Onasanya <adetola.onasanya@gmail.com>
2019-06-30 17:02:19 +0100

    Setup docker

--- test/account.mutation.e2e-spec.ts
+++ test/account.mutation.e2e-spec.ts
@@ -23,41 +23,43 @@ const signupMutation = `
 //   }
 // `;
 
-describe('AccountMutation - signup (e2e)', () => {
+describe('AccountMutation (e2e)', () => {
   afterEach(async () => {
     //reset prisma db especially the account table
     await getPrismaTestInstance().deleteManyAccounts();
   });
 
-  it('should create an account when the mutation is valid.', async () => {
-    console.log(process.env.NOD_ENV);
-    const response = await client.request(signupMutation, {
-      email: 'adetoola@gmail.com',
-      username: 'adetoola',
-      password: 'qwerty1234',
-    });
-
-    expect(response).toMatchObject({
-      signup: {
-        id: expect.any(String),
+  describe('signup', () => {
+    it('should create an account when the mutation is valid.', async () => {
+      console.log(process.env.NOD_ENV);
+      const response = await client.request(signupMutation, {
+        email: 'adetoola@gmail.com',
         username: 'adetoola',
-        email: 'adetoola@gmail.com',
-      },
-    });
-  });
+        password: 'qwerty1234',
+      });
 
-  it('should create an account when username is absent.', async () => {
-    const response = await client.request(signupMutation, {
-      email: 'adetoola@gmail.com',
-      password: 'qwerty1234',
+      expect(response).toMatchObject({
+        signup: {
+          id: expect.any(String),
+          username: 'adetoola',
+          email: 'adetoola@gmail.com',
+        },
+      });
     });
 
-    expect(response).toMatchObject({
-      signup: {
-        id: expect.any(String),
-        username: null,
+    it('should create an account when username is absent.', async () => {
+      const response = await client.request(signupMutation, {
         email: 'adetoola@gmail.com',
-      },
+        password: 'qwerty1234',
+      });
+
+      expect(response).toMatchObject({
+        signup: {
+          id: expect.any(String),
+          username: null,
+          email: 'adetoola@gmail.com',
+        },
+      });
     });
   });
 });
